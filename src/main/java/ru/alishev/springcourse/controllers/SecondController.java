7eee77277e02da19532f5a22e510cfa7c190693c
noslav7 <noslav7@gmail.com>
2023-02-13 19:48:29 +0500

    lesson 22

--- src/main/java/ru/alishev/springcourse/controllers/SecondController.java
+++ src/main/java/ru/alishev/springcourse/controllers/SecondController.java
@@ -4,8 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 public class SecondController {
 
-    @GetMapping("/exit")
-    public String exit() {
-        return "/second/exit";
-    }
+//    @GetMapping("/exit")
+//    public String exit() {
+//        return "/second/exit";
+//    }
 }
