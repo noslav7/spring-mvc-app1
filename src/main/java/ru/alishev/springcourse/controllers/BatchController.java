1800ebc2aee407fbd9ff18c09879ba133fe6853f
noslav7 <noslav7@gmail.com>
2023-02-20 08:40:57 +0500

    lesson 30 - another fix

--- src/main/java/ru/alishev/springcourse/controllers/BatchController.java
+++ src/main/java/ru/alishev/springcourse/controllers/BatchController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import ru.alishev.springcourse.dao.PersonDAO;
 
 @Controller
-@RequestMapping("test-batch-update")
+@RequestMapping("/batch/test-batch-update")
 public class BatchController {
     private final PersonDAO personDAO;
 
