63aa1981384602ff8c879c0ebe4986814dcc1135
noslav7 <noslav7@gmail.com>
2023-02-20 05:00:44 +0500

    lesson 30

---
+++ src/main/java/ru/alishev/springcourse/controllers/BatchController.java
@@ -0,0 +1,35 @@
+package ru.alishev.springcourse.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.alishev.springcourse.dao.PersonDAO;
+
+@Controller
+@RequestMapping("test-atch-update")
+public class BatchController {
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public BatchController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping("/batch/index")
+    public String index() {
+        return "test-batch-update";
+    }
+
+    @GetMapping("/without")
+    public String withoutBatch() {
+        personDAO.testMultipleUpdate();
+        return "redirect:/people";
+    }
+
+    @GetMapping("/with")
+    public String withBatch() {
+        personDAO.testMultipleUpdate();
+        return "redirect:/people";
+    }
+}
--- src/main/java/ru/alishev/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/alishev/springcourse/dao/PersonDAO.java
@@ -1,9 +1,14 @@
 package ru.alishev.springcourse.dao;
 
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import ru.alishev.springcourse.models.Person;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -36,5 +41,55 @@ public class PersonDAO {
 
     public void delete(int id) {
         jdbcTemplate.update("DELETE FROM person WHERE id=?", id);
+    }
+
+    public void  testMultipleUpdate() {
+        List<Person> people = create1000people();
+
+        long before = System.currentTimeMillis();
+
+        for (Person person : people) {
+        jdbcTemplate.update("INSERT INTO person VALUES(?, ?, ?, ?)", person.getId(),
+                person.getName(), person.getAge(), person.getEmail());
+        }
+
+        long after = System.currentTimeMillis();
+
+        System.out.println("Time: " + (after - before));
+    }
+
+    public void testBatchUpdate() {
+        List<Person> people = create1000people();
+
+        long before = System.currentTimeMillis();
+
+        jdbcTemplate.batchUpdate("INSERT INTO people VALUES(?, ?, ?, ?)",
+                new BatchPreparedStatementSetter() {
+                    @Override
+                    public void setValues(PreparedStatement preparedStatement, int i) throws SQLException {
+                        preparedStatement.setInt(1, people.get(i).getId());
+                        preparedStatement.setString(2, people.get(i).getName());
+                        preparedStatement.setInt(3, people.get(i).getAge());
+                        preparedStatement.setString(4, people.get(i).getEmail());
+                    }
+
+                    @Override
+                    public int getBatchSize() {
+                        return people.size();
+                    }
+                });
+
+        long after = System.currentTimeMillis();
+
+        System.out.println("Time: " + (after - before));
+    }
+
+    private List<Person> create1000people() {
+        List<Person> people = new ArrayList<>();
+
+        for (int i = 0; i < 1000; i++) {
+            people.add(new Person(i, "Name" + i, 30, "test" + i + "@mail.ru"));
+        }
+        return people;
     }
 }
